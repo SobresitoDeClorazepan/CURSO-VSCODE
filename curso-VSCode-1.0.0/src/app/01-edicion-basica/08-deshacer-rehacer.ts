872d1c84a252eceb6383ba63a0de200012cd56ce
SobresitoDeClorazepan <124885618+SobresitoDeClorazepan@users.noreply.github.com>
2023-02-15 22:59:18 -0500

    Add files via upload

---
+++ curso-VSCode-1.0.0/src/app/01-edicion-basica/08-deshacer-rehacer.ts
@@ -0,0 +1,23 @@
+/*
+    Objetivo:
+        Deshacer y rehacer el código
+§
+    Tips:
+        ⌘ Z
+        ⌘ ⇧ Z
+        Ctrl +Z
+        Ctrl + Shift + Z
+
+*/
+
+hola mundito xd si (eso lo borre y lo hice de nuevo, alto pendejo)
+
+
+
+
+// Demo
+// function holaMundo() {
+//     return 'Saludos a todos!';
+// }
+
+
